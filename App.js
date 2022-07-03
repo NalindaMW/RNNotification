d68513d0a91a61d0370b48f5032c8c210f7fcd43
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-07-03 22:09:03 +0530

    + expo notification installed + Handled receiving local notifications

--- App.js
+++ App.js
@@ -1,10 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    console.log("setNotificationHandler");
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("Notification Recieved");
+        console.log(notification);
+        const username = notification.request.content.data.name;
+        console.log(username);
+      }
+    );
+
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log("Notification response received");
+        console.log(response);
+      }
+    );
+
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
+  async function allowsNotificationsAsync() {
+    console.log("allowsNotificationsAsync");
+    const settings = await Notifications.getPermissionsAsync();
+    console.log("settings");
+    console.log(settings);
+
+    return (
+      settings.granted ||
+      settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+    );
+  }
+
+  async function scheduleNotificationHandler() {
+    const hasPushNotificationPermissionGranted =
+      await allowsNotificationsAsync();
+
+    if (!hasPushNotificationPermissionGranted) {
+      return;
+    }
+
+    console.log("hasPushNotificationPermissionGranted: ");
+    console.log(hasPushNotificationPermissionGranted);
+
+    if (hasPushNotificationPermissionGranted) {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "My first notification title",
+          body: "This is the notification body",
+          data: { name: "Nalinda W." },
+        },
+        trigger: {
+          seconds: 5,
+        },
+      });
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +89,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
